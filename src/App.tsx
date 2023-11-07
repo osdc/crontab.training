fe7b66256466e884b4c4f367a7ca7594cdaa8956
Rafey Ahmad <rafeyahmad@protonmail.com>
2023-11-08 00:54:27 +0530

    Generate * 70% of time for each placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,23 @@ import './App.css'
 import cronstrue from 'cronstrue'
 
 function App() {
+  function generateCronExpression() {
+    function getRandomOrWildcard(min: number, max: number) {
+      if (Math.random() < 0.7) {
+        return '*';
+      } else {
+        return getRandomIntInclusive(min, max);
+      }
+    }
+    const minute = getRandomOrWildcard(0, 59);
+    const hour = getRandomOrWildcard(0, 23);
+    const dayOfMonth = getRandomOrWildcard(1, 31);
+    const month = getRandomOrWildcard(1, 12);
+    const dayOfWeek = getRandomOrWildcard(0, 6);
+
+    return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`
+  }
+
   const cronExpression = generateCronExpression();
 
   return (
@@ -17,18 +34,6 @@ function getRandomIntInclusive(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-}
-
-
-function generateCronExpression() {
-  const minute = getRandomIntInclusive(0, 59);
-  const hour = getRandomIntInclusive(0, 23);
-  const dayOfMonth = getRandomIntInclusive(1, 31);
-  const month = getRandomIntInclusive(1, 12);
-  const dayOfWeek = getRandomIntInclusive(0, 6);
-
-
-  return `${minute} ${hour} ${dayOfMonth} ${month} ${dayOfWeek}`
 }
 
 export default App
